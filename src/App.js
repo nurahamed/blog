71abcd5b8902c1fa5b11a589f19281b4e6ac22ce
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-09-02 03:26:15 +0530

    api created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import Topbar from "./components/topbar/Topbar";
+
 import Homepage from "./pages/homepage/Homepage.";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Settings from "./pages/settings/Settings";
+import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-import ReactDOM from "react-dom/client";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+// import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const user = false;
+  const user = true;
   return (
     <>
      
@@ -17,8 +18,8 @@ function App() {
       <Topbar />
         <Routes>
           <Route exact path="/" element={<Homepage />} />
-          <Route exact path="/login" element={user? <Homepage />:<Login />} />
           <Route path="/register" element={user? <Homepage />:<Register />} />
+          <Route exact path="/login" element={user? <Homepage />:<Login />} />
           <Route path="/write" element={user ? <Write/> : <Register/>} />
           <Route path="/settings" element={user ? <Settings />: <Register/>} />
            <Route path="/post/:postId" element={<Single />} /> 
--- src/pages/homepage/Homepage..jsx
+++ src/pages/homepage/Homepage..jsx
@@ -1,12 +1,22 @@
 // import { useLocation } from "react-router";
+// import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
-// import Posts from "../../components/posts/Posts";
-// import Sidebar from "../../components/sidebar/Sidebar";
+
 import "./homepage.css";
+import axios from "axios"
 
 export default function Homepage() {
+  // const [post, setPost] = useState([]);
+  // useEffect(()=>{
+  //   const fetchPosts = async ()=>{
+  //    const res = await axios.get("/posts")
+  //    console.log(res)
+  //   }
+  //   fetchPosts()
+  // },[])
+
 //   const location = useLocation();
 //   console.log(location);
   return (
